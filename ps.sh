35fe0c1db7cb7efe8db31413a4828f3294fad894
UtrGrd <93576293+UtrGrd@users.noreply.github.com>
2022-09-21 11:21:54 +0300

    добавил скрипты
    Выполнение ДЗ

---
+++ ps.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+frmt="%-15s%-15s%-30s\t%-20s\n"
+printf "$fmt" PID USER COMM STAT
+
+for proc in `ls /proc/ | egrep "^[0-9]" | sort -n`
+do
+
+    if [[ -f /proc/$proc/status ]]
+        then
+        PID=$proc
+    COMM=`cat /proc/$proc/cmdline`
+    if  [[ -z "$COMM" ]]
+        then
+        COMM="[`awk '/Name/{print $2}' /proc/$proc/status`]"
+    else
+        COMM=`cat /proc/$proc/cmdline`
+    fi
+
+    User=`awk '/Uid/{print $2}' /proc/$proc/status`
+    Stat=`cat /proc/$proc/status | awk '/State/{print $2}'`
+    if [[ User -eq 0 ]]
+       then
+       UserName='root'
+    else
+       UserName=`grep $User /etc/passwd | awk -F ":" '{print $1}'`
+    fi
+    printf "$frmt" $PID $UserName "$COMM" $Stat
+    fi
+done
